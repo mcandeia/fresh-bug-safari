0ff70c65410d5d9a77bb44ad31f5d9d0aeda51b1
Marcos Candeia <marrcooos@gmail.com>
2023-05-16 16:08:09 -0300

    Button bug on safari
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,16 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
+
+console.log("start sleep");
+await sleep();
+console.log("end sleep");
+export const Button = (props: JSX.HTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
     />
   );
-}
+};
